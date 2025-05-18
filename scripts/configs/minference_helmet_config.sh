7c5ce10e5e1639d4dced47bdf20a8b399fd3fc93
joie-zhang <107374257+joie-zhang@users.noreply.github.com>
2025-05-18 05:20:29 -0400

    more configs for scripts

---
+++ scripts/configs/minference_helmet_config.sh
@@ -0,0 +1,41 @@
+# # baseline and minference_helmet_config.sh
+# # declare -a BASE_CONFIGS=("cite" "rerank" "recall_jsonkv" "rag_nq" "rag_hotpotqa" "niah")
+# declare -a BASE_CONFIGS=("niah")
+# declare -a CONTEXT_LENGTHS=("16k" "32k")
+# declare -a MODELS=("Llama-3.1-8B-Instruct")
+# # declare -a MODELS=("Qwen2.5-7B-Instruct")
+# # declare -a QUANTIZE=(16)  # Add quantization options
+# declare -a QUANTIZE=("")  # non-baseline experiments should not support quantize
+# # EXP_TYPE="baseline"
+# EXP_TYPE="minference"
+# BENCHMARK="helmet"
+# SEED=42
+
+# baseline and minference_longproc_config.sh
+# declare -a BASE_CONFIGS=("html_to_tsv" "pseudo_to_code")
+# declare -a CONTEXT_LENGTHS=("0.5k")
+# declare -a BASE_CONFIGS=("html_to_tsv" "pseudo_to_code" "travel_planning")
+# declare -a CONTEXT_LENGTHS=("2k")
+# declare -a BASE_CONFIGS=("html_to_tsv" "travel_planning")
+declare -a BASE_CONFIGS=("html_to_tsv")
+declare -a CONTEXT_LENGTHS=("8k")
+declare -a MODELS=("Llama-3.1-8B-Instruct")
+# declare -a MODELS=("Qwen2.5-7B-Instruct")
+# declare -a QUANTIZE=(16)  # Add quantization options
+declare -a QUANTIZE=("")  # non-baseline experiments should not support quantize
+EXP_TYPE="minference"
+BENCHMARK="longproc"
+SEED=42
+
+# SLURM Configuration
+JOB_TIME="10:00:00"
+JOB_NAME="${EXP_TYPE}_${BENCHMARK}_${CONTEXT_LENGTHS[-1]}_eval"
+
+# Export variables so they're available to the job script
+export BASE_CONFIGS
+export CONTEXT_LENGTHS
+export MODELS
+export QUANTIZE
+export EXP_TYPE
+export BENCHMARK
+export SEED
